ba66462b5e887cdd995b52bfa4bdb3e020ec0cf4
lai <elizamariebolongaita1@gmail.com>
2026-05-09 17:30:29 +0800

    docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,23 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
+use Illuminate\Support\Facades\URL;
+    class AppServiceProvider extends ServiceProvider
+    {
+/**
+* Register any application services.
+*/
     public function register(): void
     {
         //
     }
-
-    /**
-     * Bootstrap any application services.
-     */
+/**
+* Bootstrap any application services.
+*/
     public function boot(): void
     {
-        $this->app['router']->aliasMiddleware('role', \App\Http\Middleware\RoleMiddleware::class);
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        }
     }
-}
+    }
